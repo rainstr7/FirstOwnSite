fe0d7a008588414c9dd520a3171924b24816f24c
ihahn <rainstr7@gmail.com>
2019-09-10 23:10:34 +0300

    ver JS->PHP

--- puzzle.php
+++ puzzle.php
@@ -4,31 +4,7 @@
 	<meta charset="utf-8">
 	<title>Личный сайт ihahn</title>
 	<link rel="stylesheet" href="style.css">
-	<script type="text/javascript">
-
-		var score = 0;
-
-		function checkAnswer(inputId, answers){
-			var userAnswer = document.getElementById(inputId).value;
-			userAnswer = userAnswer.toLowerCase();
-			for (var i = 0; i < answers.length; i++){
-				if (userAnswer == answers[i]){
-					score++;
-					break;
-				}
-			}
-		}
-		
-		function checkAnswers(){
-
-			checkAnswer("userAnswer1", ["сон", "сновидение"]);
-			checkAnswer("userAnswer2", ["морской", "укус акулы"]);	
-			checkAnswer("userAnswer3", ["шахматный", "мертвый"]);
-
-			alert("Вы отгадали " + score + " загадок");
-		}
-
-	</script>
+	
 </head>
 <body>
 
@@ -43,16 +19,36 @@
 			<h1>Игра в загадки</h1>
 
 			<div class="box">
+			<?php
 
-				<p>Что можно увидеть с закрытыми глазами?</p>
-				<input type="text" id="userAnswer1">
-				<p>Какой болезнью никто не болеет на суше?</p>
-				<input type="text" id="userAnswer2">
-				<p>Какой конь не ест овса?</p>
-				<input type="text" id="userAnswer3">
-				<br>
-				<a href="#" onclick="checkAnswers();">Ответить</a>
+				if (isset($_GET['userAnswer1']) && isset($_GET['userAnswer2']) && isset($_GET['userAnswer3'])){
+					$userAnswer = $_GET[userAnswer1];
+					$score = 0;
+					if ($userAnswer == "сон" || $userAnswer == "сновидение"){
+						$score++;
+					}
+		    		$userAnswer = $_GET[userAnswer2];
+					if ($userAnswer == "морской" || $userAnswer == "укус акулы"){
+						$score++;
+					}		
+					$userAnswer = $_GET[userAnswer3];
+					if ($userAnswer == "шахматный" || $userAnswer == "мертвый"){
+						$score++;
+					}	
 
+					echo "Вы угадали " . $score . " загадок";
+				}	
+			?>
+				<form method="GET">
+					<p>Что можно увидеть с закрытыми глазами?</p>
+					<input type="text" name="userAnswer1">
+					<p>Какой болезнью никто не болеет на суше?</p>
+					<input type="text" name="userAnswer2">
+					<p>Какой конь не ест овса?</p>
+					<input type="text" name="userAnswer3">
+					<br>
+					<input type="submit" value="Ответить" name="">
+				</form>		
 			</div>
 
 		</div>
